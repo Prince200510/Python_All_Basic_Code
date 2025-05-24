581e51f1c6afc89767d2322a505747d476392a35
Prince200510 <princemaurya8879@gmail.com>
2025-05-24 21:43:26 +0530

    'v1.1'

---
+++ Simple_Calculator.py
@@ -0,0 +1,7 @@
+num1 = int(input('Enter a First number :'))
+num2 = int(input('Enter a Second number :'))
+
+print('Add :', num1 + num2)
+print('Sub :', num1 - num2)
+print('Mul :', num1 * num2)
+print('Div :', num1 / num2)
