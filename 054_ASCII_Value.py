2900ce8233d7b90623e5ac1228c03b6495e89f46
Prince200510 <princemaurya8879@gmail.com>
2025-05-27 15:10:32 +0530

    'Day_4'

---
+++ 054_ASCII_Value.py
@@ -0,0 +1,10 @@
+# Character
+char = input('Enter a Character :')
+ascii_value = ord(char)
+print('ASCII Value :', ascii_value)
+
+# String
+string = "Prince"
+for char in string:
+    ascii = ord(char)
+    print(ascii)
