3981efd3c2ace769524e02a49dd355f53196f317
Prince200510 <princemaurya8879@gmail.com>
2025-05-27 19:39:37 +0530

    Day_4

---
+++ 052_Volume_of_a_Cylinder.py
@@ -0,0 +1,4 @@
+radius = 25
+height = 10
+volume = 3.14 * radius * radius * height
+print('Volume of the Cylinder:', volume)
---
+++ 054_sort_list_of_string.py
@@ -0,0 +1,3 @@
+strings = ['apple', 'mango', 'cherry', 'date', 'prince']
+sorted_string = sorted(strings)
+print(sorted_string)
